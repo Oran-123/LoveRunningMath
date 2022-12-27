5a53328102cdf9c5a8652b3a2172333d7fc3712b
Oran-123 <orandillon123@gmail.com>
2022-12-27 23:09:31 +0000

    add variables to rungame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button")
 
+    for (let button of buttons){
+        button.addEventListener("click",function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("you clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type")
+                alert(`you clicked ${gameType}`)
+            }
+        })
+    }
+})
+
+/**
+ * The main game "loop",called when the script is first loaded 
+ * and after the user's answer has been processed
+ */
+function runGame(){
+ let num1 = Math.floor(Math.random()*25)+1;
+ let num2 = Math.floor(Math.random()*25)+1;
 }
 
 function checkAnswer() {
@@ -23,5 +43,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
